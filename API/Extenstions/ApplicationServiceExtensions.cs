6390b1cd8403f48e03032c4a4dffbc9bc75e7b46
Nosson Weissman <nossonweissman1@gmail.com>
2021-06-03 13:02:39 -0400

    End of part 8

--- API/Extenstions/ApplicationServiceExtensions.cs
+++ API/Extenstions/ApplicationServiceExtensions.cs
@@ -4,6 +4,8 @@ using API.Controllers.Services;
 using API.Data;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using API.Helpers;
+
 namespace API.Extenstions
 {
     public static class ApplicationServiceExtensions
@@ -11,6 +13,8 @@ namespace API.Extenstions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NODATA } from 'dns';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
